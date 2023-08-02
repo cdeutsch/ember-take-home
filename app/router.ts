5adaa983ab9727328e9fad280a620c581deef7e0
Christopher Deutsch <cd@cdeutsch.com>
2023-08-02 13:21:24 -0500

    Add TypeScript.

--- app/router.ts
+++ app/router.ts
@@ -2,7 +2,7 @@ import EmberRouter from '@ember/routing/router';
 import config from 'takehome/config/environment';
 
 export default class Router extends EmberRouter {
-  location = config.locationType;
+  location = config.locationType as EmberRouter["location"];
   rootURL = config.rootURL;
 }
 
